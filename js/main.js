a10dd5b4f26ee9042e11996ca0b738bcf9ca6a5c
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-03-22 18:33:34 +0800

    自我挑戰、修改輪播、課程介紹第一階段

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const elemCarousel = document.querySelector('#Carousel');
-let carouselIndex = 0;
-
-let bnrTimer = setInterval(setCarousel, 5000);
-function setCarousel() {
-  carouselIndex >= 3 ? carouselIndex = 0 : carouselIndex += 1;
-  elemCarousel.style.transform = `translateX(${-100 * carouselIndex}%)`
-}
